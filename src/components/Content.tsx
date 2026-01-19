66205aa49aac72326e49fa0f6ca5fdabab7746c6
Connorbelez <connor.belez@gmail.com>
2026-01-18 21:39:25 -0500

    TUI Mockup

---
+++ src/components/Content.tsx
@@ -0,0 +1,32 @@
+import { NewModelForm } from "./NewModelForm";
+import { ModelSelection } from "./ModelSelection";
+import { ModelDetails } from "./ModelDetails";
+import type { SelectOption } from "@opentui/core";
+import { useFocusState, useFocusContext } from "@/hooks/FocusProvider";
+
+export type ContentProps = {
+    models: SelectOption[];
+    selectedModel: SelectOption;
+    setSelectedModel: (model: SelectOption) => void;
+    onSelect: (model: SelectOption) => void;
+    onSave: (model: SelectOption) => void;  
+
+}
+
+export function Content({ models, selectedModel, onSelect, setSelectedModel, onSave }: ContentProps) {
+    const { focusedId } = useFocusContext();
+    if (focusedId === 'model_selection' || focusedId === 'model_details') {
+    return (
+        <box justifyContent="center" alignItems="flex-start" flexDirection="row" gap={1} width="100%" height="80%">
+            <ModelSelection models={models} onSelect={setSelectedModel} selectedModel={selectedModel} />
+            <ModelDetails model={selectedModel} onSave={onSave} />
+        </box>
+        )
+    }
+
+    return (
+        <box justifyContent="center" alignItems="flex-start" flexDirection="row" gap={1} width="100%" height="100%">
+            <NewModelForm />
+        </box>
+    );
+}
